41d30d67858bb8d7fa4056608871debd77166e48
santoshingale <santoshingale876@gmail.com>
2019-12-11 12:38:38 +0530

    Added: daily limit for win and loss

--- gamblingSimulator.sh
+++ gamblingSimulator.sh
@@ -3,13 +3,25 @@ echo "Welcome to the GAMBLING SIMULATOR"
 
 #constant
 BET=1
-STAKE=100
+stake=100
+function getDailyResult(){
 
-result=$(( RANDOM % 2))
-case $result in
-	1)
-		echo "win 1$";;
+	DAILY_LOW_LIMIT=$(( $stake / 2 ))
+	DAILY_HIGH_LIMIT=$(($stake / 2 + $stake))
 
-	0)
-		echo "loss 1$";;
-esac
+	while [ $DAILY_LOW_LIMIT -lt $stake ] && [ $DAILY_HIGH_LIMIT -gt $stake ]
+	do
+		result=$(( RANDOM % 2))
+		case $result in
+			1)
+				((stake++));;
+
+			0)
+				((stake--));;
+		esac
+	done
+
+	echo $stake
+}
+
+getDailyResult
