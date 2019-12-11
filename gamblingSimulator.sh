c15903837c7478fa2bfe94847cffbf3bdb9264d9
santoshingale <santoshingale876@gmail.com>
2019-12-11 15:23:00 +0530

    Added: func. to calculate 20 days game play

--- gamblingSimulator.sh
+++ gamblingSimulator.sh
@@ -3,25 +3,42 @@ echo "Welcome to the GAMBLING SIMULATOR"
 
 #constant
 BET=1
+DAYS=20
+
+declare -A gameRecord
+
 stake=100
 function getDailyResult(){
 
-	DAILY_LOW_LIMIT=$(( $stake / 2 ))
-	DAILY_HIGH_LIMIT=$(($stake / 2 + $stake))
-
-	while [ $DAILY_LOW_LIMIT -lt $stake ] && [ $DAILY_HIGH_LIMIT -gt $stake ]
+	for(( day=1; day<=$DAYS; day++))
 	do
-		result=$(( RANDOM % 2))
-		case $result in
-			1)
-				((stake++));;
+		stake=100
+		wins=0
+		losses=0
+		DAILY_LOW_LIMIT=$(( $stake / 2 ))
+		DAILY_HIGH_LIMIT=$(($stake / 2 + $stake))
 
-			0)
-				((stake--));;
-		esac
+		while [ $DAILY_LOW_LIMIT -lt $stake ] && [ $DAILY_HIGH_LIMIT -gt $stake ]
+		do
+			result=$(( RANDOM % 2))
+			case $result in
+				1)
+					((stake++))
+					((wins++));;
+
+				0)
+					((stake--))
+					((losses++));;
+			esac
+		done
+		outCome=$(( $stake - 100 ))
+		gameRecord["Day_$day"]=$wins" "$losses
 	done
-
-	echo $stake
 }
 
 getDailyResult
+
+for i in ${!gameRecord[@]}
+do
+	echo "$i ${gameRecord[$i]}"
+done | sort -k2 -n
