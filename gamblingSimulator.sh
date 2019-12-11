c4a9befb84043e46797f8eb9c177662e4d8600ea
santoshingale <santoshingale876@gmail.com>
2019-12-11 18:56:54 +0530

    Added: function to find lucky and unlucky day

--- gamblingSimulator.sh
+++ gamblingSimulator.sh
@@ -8,7 +8,7 @@ DAYS=20
 declare -A gameRecord
 
 stake=100
-function getDailyResult(){
+function setDailyResult(){
 
 	for(( day=1; day<=$DAYS; day++))
 	do
@@ -32,11 +32,27 @@ function getDailyResult(){
 	done
 }
 
-getDailyResult
+function getDailyProfit(){
+	for((j=1;j<=20;j++))
+	do
+		echo "Day_$j ${gameRecord[Day_$j]}"
+	done 
+}
+function getLuckyUnlucky(){
+	gameRecord[Day_0]=0
+	for((j=1;j<=20;j++))
+   do
+		k=$(( $j - 1 ))
+      gameRecord[Day_$j]=$(( ${gameRecord[Day_$j]} + ${gameRecord[Day_$k]} ))
+		echo  "Day$j	"${gameRecord[Day_$j]}
+   done | sort -k2 -nr |awk 'NR==20{print "Unluckiest: " $0}AND NR==1{print "Luckiest: " $0}'
 
-for((j=1;j<=20;j++))
-do
-	echo "Day_$j ${gameRecord[Day_$j]}"
-done 
+}
 
-echo "Total profit" $( printf "%s\n" ${gameRecord[@]} | awk '{sum+=$0}END{print sum}')
+function main(){
+	setDailyResult 
+	getDailyProfit
+	getLuckyUnlucky
+	echo "Total profit" $( printf "%s\n" ${gameRecord[@]} | awk '{sum+=$0}END{print sum}')
+}
+main
